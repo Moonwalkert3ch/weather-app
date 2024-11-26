3d94fa85807c9a04e48b487f0b1a9464342fb34b
Moonwalkert3ch <6620@holbertonstudents.com>
2024-11-26 17:28:19 -0600

    saved test

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,7 +3,7 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    console.log("Hello World!")
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
